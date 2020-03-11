59a1f46c92b8299733ac1bd2c44bdcb77e3421ac
yyjqr <yyjqr789@sina.com>
2020-03-11 16:59:01 +0800

    #6 支持多客户端连接交互的服务器程序

---
+++ robot/Eangel_server_thread_v1.6.py
@@ -0,0 +1,176 @@
+#import necessary package
+# encoding: utf-8
+##基于python3 采用线程，socket支持多客户端连接
+# 202003  Jack   yyjqr789@sina.com
+# 部分参考 https://www.cnblogs.com/liyang93/p/9117387.html
+#codeing:gb18030
+import socket
+import time
+import sys,subprocess
+import RPi.GPIO as GPIO
+import serial
+import logging    #20180318 JACK
+import datetime
+from threading import Thread #20190307 JACK
+
+conn_list = []
+conn_dt = {}
+
+
+def get_ip_address():
+        s =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        s.connect(("1.1.1.1",80))
+        ipaddr=s.getsockname()[0]
+        s.close()
+        return ipaddr
+
+#define host ip: Rpi's IP
+#HOST_IP = "192.168.0.101"
+HOST_IP=get_ip_address()
+print (HOST_IP)
+HOST_PORT = 6868
+
+print("Starting socket: TCP...")
+#1.create socket object:socket=socket.socket(family,type)
+socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("TCP server listen @ %s:%d!" %(HOST_IP, HOST_PORT) )
+host_addr = (HOST_IP, HOST_PORT)
+
+#2.bind socket to addr:socket.bind(address)
+socketTCP.bind(host_addr)
+#3.listen connection request:socket.listen(backlog)
+socketTCP.listen(5)
+#4.waite for client:connection,address=socket.accept()
+#socket_con, (client_ip, client_port) = socket_tcp.accept()
+#print("Connection accepted from %s." %client_ip)
+#socket_con.send("Welcome to RasPi TCP server!")
+
+#5.handle
+GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(11,GPIO.OUT)
+print("Receiving package...")
+
+#ser=serial.Serial("/dev/ttyUSB0",115200)
+ser=serial.Serial("/dev/UNO",115200)
+log_file ="%s%s%s"% ('EangelGo_new',datetime.date.today(),'.log')
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+
+handler = logging.FileHandler(log_file)  #"Eangel log.txt"
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+
+logger.addHandler(handler)
+logger.addHandler(console)
+
+logger.info("Eangel")
+logger.info("Start print log")
+logger.info(host_addr)
+logger.info(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))   #20180318
+logger.debug("Do something")
+#logger.warning("Something maybe fail.")
+#logger.info("logFinish")
+
+def ReceiveDataThread(socket_tcp,address): 
+    print ("Rev data....")
+    while True:    #add 20200311!!!    
+        try:
+                #socket_tcp.settimeout(5)#500----->5s
+                data=socket_tcp.recv(8192)
+                #print ("Rev data ...")
+                if len(data)>0:
+                    print("Received:%s" %data)
+                    logger.info(data)
+                    if  data=='F':
+                       # GPIO.output(11,GPIO.HIGH)
+                       ser.write('F')
+                       print("car go Forward")
+                    elif data=='L':
+                       # GPIO.output(11,GPIO.HIGH)
+                       ser.write('L')
+                       print("car turn left")
+
+                    elif data=='R':
+                       # GPIO.output(11,GPIO.LOW)
+                    #socket_con.send(data)
+                       ser.write('R')
+                       print("car turn Right")
+                    elif data=='B':
+                       ser.write('B')
+                       print("car turn Back")
+                    elif data=='P':
+                       ser.write('P')
+                       print("car Pause")
+                    elif data=='S':    #slow down  add 202003
+                       ser.write('S')
+                       print("car SLOW DOWN!")   
+        # "C" 20200301ADD            
+                    elif data=='C':
+                       #os.system('~/home/pi/camCap/source/cam10min/camFS10m_opt')
+                       subprocess.Popen('~/camCap/source/cam1min/camFS1min', shell = True, stdout = subprocess.PIPE)
+                       print("Take picture 1min !")
+                       logger.info("Take picture")
+                    elif data=='M':
+                       #subprocess.popen('mplayer /home/pi/Music/Soundtrack - Define Dancing.mp3')
+                       subprocess.Popen(["mplayer", "-slave", "-quiet", "/home/pi/Music/Soundtrack - Define Dancing.mp3"], stdin = subprocess.PIPE, stdout=open("/dev/null","w"), stderr = subprocess.PIPE, shell = False)
+                       print("Play music") 
+                       time.sleep(1)
+        # "Z" 20200307ADD    send Email        
+                    elif data=='Z':
+                       subprocess.Popen('~/sendDiffNews.sh', shell = True, stdout = subprocess.PIPE)
+                       print("SEND Email------ !")
+                       logger.info("SEND Email------ !")
+                    
+                    else:
+                       ser.flushInput()
+                       input=ser.read(1)
+                       runtime=ord(input)
+                       
+                       print (" Robot running time is %d seconds ..."%runtime)  #20180625   
+                    #continue
+        except Exception:
+                    socket_tcp.close()
+                    print(address,'offline')
+                    _index = conn_list.index(address)
+                    #gui.listBox.delete(_index)
+                    conn_dt.pop(address)
+                    conn_list.pop(_index)
+                    #sys.exit(1)
+
+def accept_client():
+    """
+    接收新连接
+    """
+    while True:
+        socket_tcp,address=socketTCP.accept()
+
+        if socket_tcp not in conn_list:
+        #client, _ = g_socket_server.accept()  # 阻塞，等待客户端连接
+            conn_list.append(address)
+            conn_dt[address] =socket_tcp
+        print ("connect from: %s:%d" % (address[0],address[1]))
+
+        # 加入连接池 20200307
+        #g_conn_pool.append(client)
+        # 给每个客户端创建一个独立的线程进行管理
+        #thread = Thread(target=message_handle, args=(client,))
+        thread=Thread(target=ReceiveDataThread,args=(socket_tcp,address))
+
+        # 设置成守护线程
+        #thread.setDaemon(True)
+        thread.start()
+
+if __name__ == '__main__':
+    #init()
+    # 新开一个线程，用于接收新连接
+    thread = Thread(target=accept_client)
+    #thread.setDaemon(True)
+    thread.start()
+    #thread.join()
+#原文链接：https://blog.csdn.net/qq_39687901/article/details/81531101
+
+
