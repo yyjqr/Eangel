0349a19a2a49dfa3635448f0cece456a709fb36d
yyjqr <pursue.19890710@live.cn>
2020-12-17 21:49:11 +0800

    Update techAI_search.py

--- pySpider/techAI_search.py
+++ pySpider/techAI_search.py
@@ -30,7 +30,7 @@ import codecs # use for write a file 0708
 my_sender='840056598@qq.com' #发件人邮箱账号，为了后面易于维护，所以写成了变量
 receiver='yyjqr789@sina.com' #收件人邮箱账号，为了后面易于维护，所以写成了变量
 #receiver=my_sender
-_pwd = "tfqlcytviyqdbcib"  #0603   #需在qq邮箱开启SMTP服务并获取授权码
+_pwd = "xxx"  #0603   #需在qq邮箱开启SMTP服务并获取授权码
 
 pin0=11
 pin1=13
