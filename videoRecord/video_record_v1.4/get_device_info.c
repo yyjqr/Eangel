1bc2a87fcb02d7819ea22961b8dc898a7760c4b6
yyjqr <yyjqr789@sina.com>
2021-02-24 22:11:20 +0800

    #2 优化存储将满，停止存储的判断

--- videoRecord/video_record_v1.4/get_device_info.c
+++ videoRecord/video_record_v1.4/get_device_info.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>  //atof
 #include <string.h>
 
 #include <unistd.h>   //sleep
@@ -28,6 +29,7 @@ void* monitor_mem_thread_proc(void* arg) {
         sleep(1);
         
         ret = commandDfResult();
+       printf("ret is %d \n",ret);
         if (ret == 0) {
 				bCapture=1;
         }
@@ -54,13 +56,24 @@ int commandDfResult()
     if(fp)
     {
         memset(buf, 0, sizeof(buf));
-        if(fgets(buf, sizeof(buf) - 1, fp) != 0)
+        //printf("fp addr: %x \n",fp);
+//从文件指针stream中读取n-1个字符，存到以str为起始地址的空间里，直到读完一行
+        if(fgets(buf, sizeof(buf) , fp) != 0)
         {
+             printf("buf: %s \n",buf);
             pclose(fp);
-            return 0;
+             double ratio;
+             ratio=atof(buf);
+            if(ratio>95)
+             {
+            printf("storage is %f,FULL!\n",ratio);
+             return -1;
+
+             }
         }
+      
     }
     pclose(fp);
 
-    return -1;
+    return 0;
 }
