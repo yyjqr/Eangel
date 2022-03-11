9943da5358404ab17634325292670ee52123a6f2
yangjie <yangjie@kctii-gba.com>
2022-03-11 17:53:18 +0800

    #完善服务端队列加入和弹出

---
+++ robot/RobotCamServer_linux/src/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "camdata.h"
+#include <time.h>
+
+deque <st_CamData> cam_deque;
+st_CamData st_oneFrame,st_sendFrame;
+unsigned int timeDuration=10;
+const int width=1280, height=720;
+const int channel=3;
+bool  b_recvStatus=false;
+bool  b_socketRecvError=false;
+
+time_t timep,t,tOne;
+    tm* local;
+
+
+#endif // MAIN_H
