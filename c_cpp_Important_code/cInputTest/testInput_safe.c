6458fccb0c75587aaa127d75806b6edc60c0eb7d
yyjqr <yyjqr789@sina.com>
2022-09-09 22:10:56 +0800

    double compute

--- c_cpp_Important_code/cInputTest/testInput_safe.c
+++ c_cpp_Important_code/cInputTest/testInput_safe.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdint.h>
+
+typedef struct Robot_motion
+{
+  double velocity;
+  
+  double  speed; //w
+  double distance;
+  double  reduce_ratio;
+}robot_motion_t;
 
 int main()
 {
@@ -13,5 +23,14 @@ int main()
    scanf("%d,%d",&boardNum,&servoNum);
   
   printf("input board num %d,servo num %d\n",boardNum,servoNum);
-return 0;
+  double vel=0.1000;
+  int16_t  speed=vel*10*60*1.0000/0.2000000;
+  printf("vel:%f,speed:%d\n",vel,speed);  
+  
+  robot_motion_t  mobile_robot;
+  mobile_robot.reduce_ratio=1.0000000;
+  mobile_robot.distance=0.2000000;
+  int16_t  speedTwo=vel*10*60*(mobile_robot.reduce_ratio)/(mobile_robot.distance);
+  printf("vel:%f,speed:%d\n",vel,speedTwo);  
+  return 0;
 }
