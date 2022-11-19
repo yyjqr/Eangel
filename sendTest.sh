0a48232bfd5096dab8ac3cced371d1c79456e08d
yyjqr <pursue.19890710@live.cn>
2022-11-19 20:34:32 +0800

    Update sendTest.sh

--- sendTest.sh
+++ sendTest.sh
@@ -4,5 +4,5 @@ cd pySpider/
 echo "search valueable info"
 mkdir techNews
 python techAI_searchValueOpt.py
-echo "search mil news"
-python spider_TechMili_Drive_OPT_V1.8.py
+#echo "search mil news"
+#python spider_TechMili_Drive_OPT_V1.8.py
