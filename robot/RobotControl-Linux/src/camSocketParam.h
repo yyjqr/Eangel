021f85e98760ebcd1b1e80d70debd39ec0197bc3
yyjqr <yangjie@gosuncn.com>
2025-05-07 14:45:20 +0800

    更新robot控制界面和json ip传入

--- robot/RobotControl-Linux/src/camSocketParam.h
+++ robot/RobotControl-Linux/src/camSocketParam.h
@@ -1,20 +1,21 @@
 #ifndef CAMSOCKETPARAM_H
 #define CAMSOCKETPARAM_H
 #include <stdint.h>
-//1280*720=921600 像素或传输图像的大小 *3
-//640*480*3=1280*720
-//#define IMAGESIZE (921600*3)
-//const int IMAGESIZE=921600;
-const int CAM_ResolutionRatio=3;
-<<<<<<< HEAD:robot/RobotControl-ThreadSocketOptQueue/src/camSocketParam.h
-const int MAX_LEN=2764800;
-enum CAM_TYPE{ Common_Type720p,Small_480p=1,Common_Type1080p};
-=======
-enum CAM_TYPE{ Small_480p=0,Common_Type720p,Common_Type1080p};
->>>>>>> master:robot/RobotControl-Linux/src/camSocketParam.h
+// 1280*720=921600 像素或传输图像的大小 *3
+// 640*480*3=1280*720
+// #define IMAGESIZE (921600*3)
+// const int IMAGESIZE=921600;
+const int CAM_ResolutionRatio = 3;
+const int MAX_LEN = 2764800;
+enum CAM_TYPE
+{
+    Common_Type720p,
+    Small_480p = 1,
+    Common_Type1080p
+};
 struct camInfo
 {
-    uint8_t *imageBuf=NULL;
+    uint8_t *imageBuf = NULL;
     int imageWidth;
     int imageHeight;
     int type;
