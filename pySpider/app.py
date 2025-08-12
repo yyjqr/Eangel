3cb36e7f3ca954499b590a268a4f1a306a00eaa1
yyjqr <yyjqr789@sina.com>
2025-08-12 22:11:44 +0800

    dash-web

---
+++ pySpider/app.py
@@ -0,0 +1,25 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)  # 添加 __name__ 参数
+
+# 修复：用 html.Div 包裹所有组件
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])  # 注意这里使用 html.Div 包裹
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)
